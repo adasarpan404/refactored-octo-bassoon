1454f82c4d976fa61bfb9406c2e41691e920a007
Arpan Das <dasarpan471@gmail.com>
2023-07-31 19:27:10 +0530

    deletion has been started

---
+++ linkedlist/deletion.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int value)
+    {
+        data = value;
+        next = NULL;
+    }
+};
