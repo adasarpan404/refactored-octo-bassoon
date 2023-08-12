b87fd6f75f5a7a9aaa549447e6c1b3eab64d39fa
Arpan Das <dasarpan471@gmail.com>
2023-08-12 16:01:27 +0530

    linkedlist implementation of queue

--- queue/queueLink.cpp
+++ queue/queueLink.cpp
@@ -1,1 +1,64 @@
 #include <bits/stdc++.h>
+
+using namespace std;
+
+class Queue
+{
+public:
+    int data;
+    Queue *next;
+    Queue(int value)
+    {
+        data = value;
+        next = NULL;
+    }
+};
+
+void enqueue(Queue *&head, int value)
+{
+    Queue *q = new Queue(value);
+    if (head == NULL)
+    {
+        head = q;
+        return;
+    }
+
+    Queue *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = q;
+}
+
+void dequeue(Queue *&head)
+{
+    if (isEmpty(head))
+    {
+        cout << "Underflow";
+    }
+    Queue *q = head;
+    head = head->next;
+    delete q;
+}
+
+int front(Queue *&head)
+{
+    return head->data;
+}
+
+bool isEmpty(Queue *&head)
+{
+    return !head;
+}
+
+int main()
+{
+    Queue *head = NULL;
+    enqueue(head, 1);
+    enqueue(head, 2);
+    enqueue(head, 3);
+    cout << front(head) << endl;
+    dequeue(head);
+    cout << front(head) << endl;
+}
