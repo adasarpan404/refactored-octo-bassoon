2aedf1e3353dee27ca95dd4e61723c495688270a
Arpan Das <dasarpan471@gmail.com>
2023-08-15 09:09:59 +0530

    levelorder traversal

--- tree/traversal/levelorder.cpp
+++ tree/traversal/levelorder.cpp
@@ -16,7 +16,49 @@ public:
     }
 };
 
+vector<int> levelOrderTraversal(Node *root)
+{
+    vector<int> ans;
+
+    if (root == NULL)
+        return ans;
+
+    queue<Node *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+
+        Node *temp = q.front();
+        q.pop();
+
+        if (temp->left != NULL)
+            q.push(temp->left);
+        if (temp->right != NULL)
+            q.push(temp->right);
+
+        ans.push_back(temp->data);
+    }
+    return ans;
+}
+
 int main()
 {
+    Node *root = new Node(0);
+    root->left = new Node(1);
+    root->right = new Node(2);
+    root->left->left = new Node(3);
+    root->left->right = new Node(4);
+    root->right->left = new Node(5);
+    root->right->right = new Node(6);
+    root->left->left->right = new Node(7);
+    root->left->left->right->left = new Node(8);
+    root->right->right->left = new Node(9);
+    vector<int> levelOrder = levelOrderTraversal(root);
+    cout << "The Level Order Traversal is : ";
+    for (int i = 0; i < levelOrder.size(); i++)
+    {
+        cout << levelOrder[i] << " ";
+    }
     return 0;
 }
